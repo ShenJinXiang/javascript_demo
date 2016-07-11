9f21bc71aa2a0ca974f73ce08189fa67972026d5
shenjinxiang <sjx-sword@163.com>
2016-07-11 17:19:30 +0800

    start canvas clock demo

---
+++ source/canvas钟表/clock.js
@@ -0,0 +1,9 @@
+(function(){
+	var width = 400;
+	var height = 400;
+
+	var canvas = document.getElementById('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	var context = canvas.getContext('2d');
+}())
