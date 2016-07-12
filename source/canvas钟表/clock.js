e0f9d615f2d72db9c149882a496904876f3b5521
shenjinxiang <sjx-sword@163.com>
2016-07-12 09:55:28 +0800

    完成刻度

--- source/canvas钟表/clock.js
+++ source/canvas钟表/clock.js
@@ -20,34 +20,59 @@
 	drawScale();
 
 	function drawFream() {
+		context.save();
 		context.beginPath();
 		context.arc(canvas.width / 2, canvas.height / 2, config.radius, 0, 2 * Math.PI, false);
-		context.lineWidth = config.borderWidth;
-		context.strokeStyle = '#000';
+		context.arc(canvas.width / 2, config.height / 2, config.radius - config.borderWidth, 0, 2 * Math.PI, true);
+		context.fillStyle = "#333";
+		context.shadowColor = "#444";
+		context.shadowBlur = 50;
 		context.closePath();
-		context.stroke();
+		context.fill();
+		context.restore();
 	}
 
 	function drawOrigin() {
+		context.save();
 		context.beginPath();
 		context.arc(canvas.width / 2, canvas.height / 2, config.origin.radius, 0, 2 * Math.PI, false);
 		context.fillStyle = config.origin.color;
 		context.closePath();
 		context.fill();
+		context.restore();
 	}
 	
 	function drawScale() {
-		debugger;
 		for(var i = 0; i < 60; i++) {
+			var obj = {
+				sx : config.radius - 15 - config.borderWidth,
+				sy : 0,
+				ex : config.radius - config.borderWidth - 5,
+				ey : 0,
+				color : "#333",
+				width : 1
+			};
 			context.save();
 			context.translate(canvas.width / 2, canvas.height / 2);
 			context.rotate(i * 6 * Math.PI / 180);
-			context.moveTo(config.radius - 10, 0);
-			context.lineTo(config.radius, 0);
-			context.lineWidth = 1;
+			context.beginPath();
+			if(i % 5 == 0) {
+				obj.width = 3;
+				obj.color = "#000";
+			}
+			if(i % 15 == 0) {
+				obj.sx = config.radius - 20 - config.borderWidth;
+			}
+			context.moveTo(obj.sx, obj.sy);
+			context.lineTo(obj.ex, obj.ey);
+			context.strokeStyle = obj.color;
+			context.lineWidth = obj.width;
 			context.closePath();
 			context.stroke();
 			context.restore();
 		}
+	}
+
+	function drawNumbers() {
 	}
 }());
